8f7e51b87785045f62ce065e396476e5c4da14ea
Velomad <sagaryc111@gmail.com>
2021-02-18 02:51:12 +0530

    scraper setup completed

--- controller/controller.scraper.js
+++ controller/controller.scraper.js
@@ -3,8 +3,7 @@ const { topSellingService } = require("../services/services.scraper");
 
 module.exports = {
   topSelling: async (req, res) => {
-    const url =
-      "https://earnkaro.com/top-selling-products/beauty-and-personal-care";
+    const url = "";
 
     function wait(ms) {
       return new Promise((resolve) => setTimeout(() => resolve(), ms));
@@ -13,7 +12,32 @@ module.exports = {
       username: "sagarc111@gmail.com",
       password: "xtrainn213",
     };
-
+    async function autoScroll(page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          // let totalHeight = 0;
+          let distance = 100;
+          let timer = setInterval(() => {
+            // let scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+            let getText = document.querySelector(
+              "#Title_Get_Extra_From_Earnkaro"
+            ).innerText;
+            if (
+              document.querySelectorAll(".fl.fw.product_c").length ===
+              Number(((getText.match(/(\d+)/)[0] / 100) * 100).toFixed(0))
+            ) {
+              console.log("in if block");
+              getText = "";
+              window.scrollTo(0, 0);
+              clearInterval(timer);
+              resolve();
+            }
+          }, 70);
+        });
+      });
+    }
     const browser = await puppeteer.launch({
       headless: false,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -26,34 +50,134 @@ module.exports = {
 
     await page.setViewport({ width: 1200, height: 768 });
 
-    await page.goto(`${url}`, {
+    await page.goto(`https://earnkaro.com/`, {
       waitUntil: "networkidle0",
     });
 
     const signIn = await page.$("#link_signin");
     await signIn.click({ clickCount: 1 });
-    await wait(2000);
+    await wait(3000);
     const signInWithFb = await page.$("div>a#close_and_go_fb_signin_pp");
     await signInWithFb.click({ clickCount: 1 });
-    await wait(2000);
+    await wait(3000);
     const pages = await browser.pages();
     const popup = pages[pages.length - 1];
-    console.log(popup);
-    await fbPage.type("#email", config.username, { delay: 30 });
-    await fbPage.type("#pass", config.password, { delay: 30 });
-    // await popup.close()
-    // const fbPage = await browser.newPage();
-    // await fbPage.goto(
-    //   "https://www.facebook.com/login.php?skip_api_login=1&api_key=442881609842304&kid_directed_site=0&app_id=442881609842304&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv3.2%2Fdialog%2Foauth%3Fapp_id%3D442881609842304%26cbt%3D1613564213811%26channel_url%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df1df04ac582bb5c%2526domain%253Dearnkaro.com%2526origin%253Dhttps%25253A%25252F%25252Fearnkaro.com%25252Ff2478038a5bb0ac%2526relation%253Dopener%26client_id%3D442881609842304%26display%3Dpopup%26domain%3Dearnkaro.com%26e2e%3D%257B%257D%26fallback_redirect_uri%3Dhttps%253A%252F%252Fearnkaro.com%252F%26locale%3Den_US%26logger_id%3Df6c02b1e8d5554%26origin%3D1%26redirect_uri%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df2104ba865f029c%2526domain%253Dearnkaro.com%2526origin%253Dhttps%25253A%25252F%25252Fearnkaro.com%25252Ff2478038a5bb0ac%2526relation%253Dopener%2526frame%253Df2d6d84bd6336f4%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26scope%3Demail%252Cpublic_profile%26sdk%3Djoey%26version%3Dv3.2%26ret%3Dlogin%26fbapp_pres%3D0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df2104ba865f029c%26domain%3Dearnkaro.com%26origin%3Dhttps%253A%252F%252Fearnkaro.com%252Ff2478038a5bb0ac%26relation%3Dopener%26frame%3Df2d6d84bd6336f4%26error%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied&display=popup&locale=en_GB&pl_dbl=0",
-    //   {
-    //     waitUntil: "networkidle0",
-    //   }
-    // );
-    // await fbPage.type("#email", config.username, { delay: 30 });
-    // await fbPage.type("#pass", config.password, { delay: 30 });
-    // const loginbutton = await fbPage.$("#loginbutton");
-    // await loginbutton.click({ clickCount: 1 });
-    // topSellingService("data");
-    // res.send("test");
+    await popup.type("#email", config.username, { delay: 30 });
+    await popup.type("#pass", config.password, { delay: 30 });
+    const loginbutton = await popup.$("#loginbutton");
+    await loginbutton.click({ clickCount: 1 });
+    await wait(5000);
+    const links = [
+      "best-of-myntra",
+      "beauty-and-personal-care",
+      "best-of-mamaearth",
+      "best-of-ajio",
+    ];
+    for (var i in links) {
+      await page.goto(`https://earnkaro.com/top-selling-products/${links[i]}`, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      // await autoScroll(page);
+
+      // // Get the height of the rendered page
+      // const bodyHandle = await page.$("body");
+      // const { height } = await bodyHandle.boundingBox();
+      // await bodyHandle.dispose();
+
+      // // Scroll one viewport at a time, pausing to let content load
+      // const viewportHeight = page.viewport().height;
+      // let viewportIncr = 0;
+      // while (viewportIncr + viewportHeight < height) {
+      //   await page.evaluate((_viewportHeight) => {
+      //     window.scrollBy(0, _viewportHeight);
+      //   }, viewportHeight);
+      //   await wait(100);
+      //   viewportIncr = viewportIncr + viewportHeight;
+      // }
+
+      let data = await page.evaluate(() => {
+        let products = [];
+        let productElements = document.querySelectorAll(".fl.fw.product_c");
+
+        productElements.forEach((productElement, index) => {
+          let productJson = {};
+
+          let imgArry = [];
+
+          var liLength = document.querySelectorAll(".product_img_thumb")[index]
+            .childNodes[1].children.length;
+          for (var j = 0; j < liLength; j++) {
+            imgArry.push(
+              document.querySelectorAll(".product_img_thumb")[index]
+                .childNodes[1].children[j].children[0].src
+            );
+          }
+
+          try {
+            // product Price
+            productJson.productPrice = productElement.querySelector(
+              ".p_totalprice"
+            )
+              ? productElement.querySelector(".p_totalprice").innerText
+              : null;
+
+            // Price Strike
+            productJson.priceStrike = productElement.querySelector(
+              ".price_strike>del"
+            )
+              ? productElement.querySelector(".price_strike>del").innerText
+              : null;
+
+            // Brand
+            productJson.brandName = productElement.querySelector(".brand")
+              ? productElement.querySelector(".brand").innerText.split(":")[1]
+              : null;
+
+            // Product Name
+            productJson.productName = productElement.querySelector(
+              ".product_name"
+            )
+              ? productElement.querySelector(".product_name").title
+              : null;
+
+            // Product Image
+            productJson.productImage = productElement.querySelector(
+              ".product_img_mn>a>img"
+            )
+              ? productElement.querySelector(".product_img_mn>a>img").src
+              : null;
+
+            // Discount Percent
+            productJson.discountPercent = productElement.querySelector(
+              ".price_percentage"
+            )
+              ? productElement.querySelector(".price_percentage").innerText
+              : null;
+
+            // Product Link
+            productJson.productLink = productElement.querySelector(
+              ".fl.share_addbtn.add_to_wishlilst"
+            )
+              ? productElement.querySelector(
+                  ".fl.share_addbtn.add_to_wishlilst"
+                ).dataset.cashback_url + "1364982"
+              : null;
+
+            productJson.thumbnailImages = imgArry;
+          } catch (e) {
+            console.log(e);
+          }
+          products.push(productJson);
+        });
+        return products;
+      });
+
+      topSellingService(data, links[i]);
+    }
+
+    res.send("Finish");
+
+    await browser.close();
   },
 };
--- services/services.scraper.js
+++ services/services.scraper.js
@@ -1,5 +1,29 @@
+const pool = require("../db");
+
 module.exports = {
-  topSellingService: (val) => {
-    console.log(val);
+  topSellingService: (data, offerZone) => {
+    console.log(data);
+
+    let arry = [];
+
+    data.map((el) => {
+      arry.push([
+        el.website,
+        el.category,
+        el.displayCategory,
+        el.gender,
+        el.productName,
+        el.brandName,
+        el.imageUrl,
+        el.discountPercent,
+        el.productPrice,
+        el.productPriceStrike,
+        el.productLink,
+        el.size,
+        el.productRating,
+      ]);
+    });
+
+    console.log(offerZone, "was extracted");
   },
 };
